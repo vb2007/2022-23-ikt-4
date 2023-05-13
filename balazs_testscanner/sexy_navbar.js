932d8bb232765b7c3c1486472d81e7e7cb03fe50
TheEvilApple <68242928+TheEvilApple@users.noreply.github.com>
2023-05-13 18:40:52 +0200

    Add files via upload

--- balazs_testscanner/sexy_navbar.js
+++ balazs_testscanner/sexy_navbar.js
@@ -1,37 +1,36 @@
 const SCROLL_THRESHOLD = 69; // px
 const NAV = document.getElementById("nav");
-
 const COLOR_CHANGE_DURATION = 500; // ms
 const COLOR_CHANGE_INTERVAL = 16; // ms
-const COLOR_CHANGE_STEP = COLOR_CHANGE_INTERVAL / COLOR_CHANGE_DURATION;
-
+const ALPHA_CHANGE_STEP = 1 / (COLOR_CHANGE_DURATION / COLOR_CHANGE_INTERVAL);
 let navColor = "blue";
+let navAlpha = 0;
 let navColorStart;
 let navColorEnd;
 
 window.addEventListener('scroll', function() {
-    if (window.scrollY >= SCROLL_THRESHOLD && navColor === "blue") {
-        navColor = "red";
-        navColorStart = performance.now();
-        navColorEnd = navColorStart + COLOR_CHANGE_DURATION;
-        requestAnimationFrame(updateNavColor);
-    }
-    else if (window.scrollY < SCROLL_THRESHOLD && navColor === "red") {
-        navColor = "blue";
-        navColorStart = performance.now();
-        navColorEnd = navColorStart + COLOR_CHANGE_DURATION;
-        requestAnimationFrame(updateNavColor);
-    }
-    });
-
-    function updateNavColor(timestamp) {
-        const progress = (timestamp - navColorStart) / COLOR_CHANGE_DURATION;
-        if (progress >= 1) {
-            NAV.style.backgroundColor = navColor;
-            return;
-        }
-
-    NAV.style.backgroundColor = `rgba(${parseInt("ff", 16)},${parseInt("00", 16)},${parseInt("ff", 16)},${progress})`;
+  if (window.scrollY >= SCROLL_THRESHOLD && navColor === "blue") {
+    navColor = "red";
+    navAlpha = 0;
+    navColorStart = performance.now();
+    navColorEnd = navColorStart + COLOR_CHANGE_DURATION;
     requestAnimationFrame(updateNavColor);
+  } else if (window.scrollY < SCROLL_THRESHOLD && navColor === "red") {
+    navColor = "blue";
+    navAlpha = 0;
+    navColorStart = performance.now();
+    navColorEnd = navColorStart + COLOR_CHANGE_DURATION;
+    requestAnimationFrame(updateNavColor);
+  }
+});
 
-    }
+function updateNavColor(timestamp) {
+  const progress = (timestamp - navColorStart) / COLOR_CHANGE_DURATION;
+  if (progress >= 1) {
+    NAV.style.backgroundColor = navColor;
+    return;
+  }
+  navAlpha += ALPHA_CHANGE_STEP;
+  NAV.style.backgroundColor = `rgba(${parseInt("ff", 16)},${parseInt("00", 16)},${parseInt("ff", 16)},${navAlpha})`;
+  requestAnimationFrame(updateNavColor);
+}
