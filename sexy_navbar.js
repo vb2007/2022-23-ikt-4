8aee2ad2e27745076e82c42d336dc33613c9f23e
TheEvilApple <vagvolgyi.balazs.vb@gmail.com>
2023-05-16 20:55:03 +0200

    Scriptek, css fájlok mappába rendezése. Kapcsolat kialakítása a fájlok közt.

--- sexy_navbar.js
+++
@@ -1,7 +0,0 @@
-const SCROLL_THRESHOLD = 100; // px
-const NAV = document.getElementsByClassName("nav")[0];
-
-window.addEventListener('scroll', function() {
-  if (this.window.scrollY >= SCROLL_THRESHOLD) NAV.classList.add("nav_gorgetett");
-  else NAV.classList.remove("nav_gorgetett");
-});
